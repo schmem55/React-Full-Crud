08b0c78ac4f2c87dbe286043519f5fd10e44e8f6
schmem55 <scemama.jeremy@gmail.com>
2020-07-15 18:21:43 +0300

    push

---
+++ src/components/edit-todo.component.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function EditTodo() {
+  return (
+    <div>
+        <p>Welcome to edit todo  component</p>
+    </div>
+  );
+}
+
+
---
+++ src/components/todos-list.component.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function TodosList() {
+  return (
+    <div>
+        <p>Welcome to TODOS list component</p>
+    </div>
+  );
+}
+
+
